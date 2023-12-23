f96df6867d25f4358dab2228d66edc9e3b9c9f48
Garik021 <142084929+Garik021@users.noreply.github.com>
2023-12-23 13:06:48 +0300

    Create style.css

---
+++ style.css
@@ -0,0 +1,24 @@
+body {
+    font-family: sans-serif;
+}
+
+h1{
+   color: grey;
+}
+a{
+ text-decoration: none;  
+}
+header{
+    background-color: gray;
+}
+footer {
+    padding-left: 250px;
+}    
+.g{
+    position: absolute;
+    left: 500px;
+    bottom: 230px;
+}
+li{
+    background-color: rgb(0, 255, 98);
+}
